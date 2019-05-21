a05c9148f875809f9e50c0e74f00618793a64ecd
Hugo <24921406+hjlopes@users.noreply.github.com>
2019-05-22 00:05:27 +0200

    Updated endpoints to have item-based recommendation

--- app/main.py
+++ app/main.py
@@ -16,24 +16,28 @@ app = Flask("recommender_api")
 def get_data(path: str):
     return pd.read_csv(path)
 
+#################################
+# User Based Collaborative Model
+#################################
 
-def _data_preparation(data):
+
+def _user_data_preparation(data: pd.DataFrame):
     tag_count_df = data.groupby(['user_id', 'tag_id']).agg({'tag_id': 'count'}).rename(columns={'tag_id': 'tag_count'})
     tag_count_df = tag_count_df.reset_index()
     user_products_df = tag_count_df.pivot(index='user_id', columns='tag_id', values='tag_count').fillna(0)
     return user_products_df
 
 
-def _euclidean_distance_model(user_tags_df: pd.DataFrame):
+def _euclidean_distance_user_model(user_tags_df: pd.DataFrame):
     item_similarity = pairwise_distances(user_tags_df.values.T, metric='euclidean')
     user_predictions = user_tags_df.values.dot(item_similarity)/np.abs(item_similarity.sum(axis=1))
     user_predictions = 1 / (1 + user_predictions)
     return pd.DataFrame(user_predictions, columns=user_tags_df.columns, index=user_tags_df.index)
 
 
-def generate_model(data):
-    user_products_df = _data_preparation(data)
-    predictions_df = _euclidean_distance_model(user_products_df)
+def generate_user_model(data: pd.DataFrame):
+    user_products_df = _user_data_preparation(data)
+    predictions_df = _euclidean_distance_user_model(user_products_df)
     return predictions_df
 
 
@@ -53,26 +57,72 @@ def get_user_predictions(data, user, predictions, n_preds, not_seen=True):
     return user_predictions[:n_preds]['tag_id'].values.tolist()
 
 
-# Load the data and prepare the model
-logger.info("Loading user data and generating predictions")
+#################################
+# Item Based Collaborative Model
+#################################
+
+
+def _tag_data_preparation(data: pd.DataFrame):
+    tag_count_df = data.groupby(['user_id', 'tag_id']).agg({'tag_id': 'count'}).rename(columns={'tag_id': 'tag_count'})
+    tag_count_df = tag_count_df.reset_index()
+    tags_user_df = tag_count_df.pivot(index='tag_id', columns='user_id', values='tag_count').fillna(0)
+    return tags_user_df
+
+
+def _cosine_distance_model(tags_user_df: pd.DataFrame):
+    item_distances = pairwise_distances(tags_user_df.values, metric='cosine')
+    item_similarity = 1 - item_distances
+    np.fill_diagonal(item_similarity, 0)
+    return pd.DataFrame(item_similarity, columns=tags_user_df.index, index=tags_user_df.index)
+
+
+def generate_tag_model(data: pd.DataFrame):
+    tag_user_data = _tag_data_preparation(data)
+    item_similarity = _cosine_distance_model(tag_user_data)
+    return item_similarity
+
+
+def get_tag_predictions(item_similarity_scores, tag, top_n):
+    items_keep = item_similarity_scores.loc[tag][item_similarity_scores.loc[tag] > 0]
+    return items_keep.sort_values(ascending=False)[:top_n].index.tolist()
+
+
+# Load the data and prepare the user model
+logger.info("Generating user predictions")
 user_data = get_data(data_path)
 user_data = user_data.set_index('user_id')
-user_predictions = generate_model(user_data)
+user_predictions = generate_user_model(user_data)
+
+logger.info("Generating user predictions")
+tag_data = get_data(data_path)
+tag_similarities = generate_tag_model(tag_data)
+tag_data = tag_data.set_index('tag_id')
+
 logger.info("Data and model generated successfully")
-
-
-@app.route('/recommend/<string:user_id>')
-def get_recommended_tags(user_id):
+@app.route('/predict-user/<string:user_id>')
+def get_user_recommended_tags(user_id):
     # Check that user exists:
     try:
         user_data.loc[user_id]
     except:
         return jsonify({"error": "The user does not exist"})
     user_predicted_tags = get_user_predictions(user_data, user_id, user_predictions, 10, not_seen=True)
-    return jsonify({"user_predicted_tags": user_predicted_tags})
+    return jsonify({"predicted_tags": user_predicted_tags})
+
+
+@app.route('/predict-tag/<string:tag_id>')
+def get_tag_recommended_tags(tag_id):
+    # Check that user exists:
+    try:
+        tag_data.loc[tag_id]
+    except:
+        return jsonify({"error": "The tag does not exist"})
+    tag_predicted_tags = get_tag_predictions(tag_similarities, tag_id, 10)
+
+    return jsonify({"predicted_tags": tag_predicted_tags})
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
 
